bc6721bdb39b34c99ea8f8f0450d9ea2b0de65db
Nikola Anastasovski <Nikola.Anastasovski@netcetera.com>
2020-10-07 20:07:26 +0200

    Added a JUnit test with MockMVC

---
+++ taco-cloud/src/test/java/sia/tacocloud/HomeControllerTest.java
@@ -0,0 +1,34 @@
+package sia.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(HomeController.class)
+public class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHomePage() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"));
+//                .andExpect((ResultMatcher) content().string(containsString("Welcome to...")));
+        System.out.println(content());
+
+    }
+}
